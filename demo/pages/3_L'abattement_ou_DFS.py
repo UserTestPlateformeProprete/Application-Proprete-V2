0c961b6e00ebccfba47e594efce773d298e25ca8
Paul JOURDAIN <paul.jourdain@etu.imt-nord-europe.fr>
2024-01-16 12:20:01 +0100

    push version test

---
+++ demo/pages/3_L'abattement_ou_DFS.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+# Configuration de la page
+st.set_page_config(
+    page_title="La déduction forfaitaire spécifique",
+    page_icon="📖",
+)
+st.title(":warning: Travail en cours :warning:")
+
+
+
+
