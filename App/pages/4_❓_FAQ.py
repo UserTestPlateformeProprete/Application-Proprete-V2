c680e8b105d8e5a05077fa2dc08c26af85960e7c
Paul JOURDAIN <paul.jourdain@etu.imt-nord-europe.fr>
2024-01-25 17:43:55 +0100

    push faq

--- App/pages/4_❓_FAQ.py
+++ App/pages/4_❓_FAQ.py
@@ -10,13 +10,12 @@ st.title("Foire Aux Questions")
 st.empty()
 
 with st.container():
-    st.header("Section 1")
-
-    with st.expander("Question 1"):
-        st.write("I#m blue")
-
-    with st.expander("Question 2"):
-        st.write("I#m blue")
+    with st.expander("Que faire si le salaire calculé ne correspond pas à mon salaire ?"):
+        st.write("""Dans ce cas, nous vous conseillons de prendre contact avec le service des ressources humaines
+                    pour obtenir des informations supplémentaires. 
+                    Calculer un salaire demande de nombreuses données, il est aussi possible 
+                    que certaines d'entre elles n’aient pas été comprises dans le calcul,
+                    ce qui peut expliquer cette différence.""")
 
     with st.expander("Question 3"):
         st.write("I#m blue")
