329c2b6b5a40694a5f4dbc9e8b8e1d2b87e665ea
Paul JOURDAIN <paul.jourdain@etu.imt-nord-europe.fr>
2024-01-25 17:44:35 +0100

    push faq

--- App/pages/4_❓_FAQ.py
+++ App/pages/4_❓_FAQ.py
@@ -17,19 +17,25 @@ with st.container():
                     que certaines d'entre elles n’aient pas été comprises dans le calcul,
                     ce qui peut expliquer cette différence.""")
 
-    with st.expander("Question 3"):
-        st.write("I#m blue")
+    with st.expander("Que faire si ma classe ne correspond pas ?"):
+        st.write("""
+        En cas de sous-classification identifiée  il est possible de :    
+        - négocier avec l’employeur en ayant éventuellement recours à un représentant du personnel ou à un Syndicat. 
+        - ou, effectuer une saisie du conseil de prud’hommes """)
 
-st.divider()
+    with st.expander("Qui puis-je contacter pour obtenir plus d’informations et être accompagné?"):
+        st.write("""
+        Vous pouvez faire appel :     
+        - à un syndiqué.    
+        - à une Association d’Avocats, des permanences gratuites existent.    
+        - à l’Inspection du travail, vous pouvez vous renseigner auprès d'eux, sans que cela amène à une enquête si 
+        vous ne souhaitez pas en faire une.""")
 
-with st.container():
-    st.header("Section 2")
+    with st.expander("Où trouver des informations sur mes droits?"):
+        st.markdown("""
+        - Vous pouvez consulter la Convention Collective ici : [Convention collective Entreprises de 
+        propreté et services associés](https://www.legifrance.gouv.fr/conv_coll/id/KALIARTI000027172464/?idConteneur=KALICONT000027172335&origin=list)    
+        - Et voici une autre page qui explique de manière simplifiée la Convention Collective : [Convention 
+        collective Propreté 2024](https://www.convention.fr/convention-proprete-entreprises-de-3173.html)
+        """)
 
-    with st.expander("Question 1"):
-        st.write("I#m blue")
-
-    with st.expander("Question 2"):
-        st.write("I#m blue")
-
-    with st.expander("Question 3"):
-        st.write("I#m blue")
